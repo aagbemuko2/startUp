1801796d3c8c14bb1d173b32122a9a67648684fd
Adedotun Agbemuko <141842525+aagbemuko2@users.noreply.github.com>
2023-08-18 12:31:52 +0000

    Add function docstrings

--- my_python_file.py
+++ my_python_file.py
@@ -1,3 +1,15 @@
 # This is a dummy script
 def dummy():
+    """
+    Apparently I needed some docstrings to say what the heck
+    this function does. Its a dummy function!
+
+    Parameters
+    ----------
+    nothing
+
+    Returns
+    -------
+    nothing
+    """
     print('I am whoever I am')
